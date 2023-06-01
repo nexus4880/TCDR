c7d2929a644264b1bff546cc242d00944c37af80
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2023-06-01 11:24:10 -0700

    Changed intptr_t to uint64_t, added box ESP (scale it yourself, 256 works fine for me), added group id, removed autistic way of converting wchar to mbchar, made Daniel's code readable, undid the sorting as it was making it difficult to debug

--- Hacks.cpp
+++ Hacks.cpp
@@ -13,9 +13,11 @@
 #include <iomanip>
 #include <format>
 
+#include <iostream>
+
 namespace Hacks {
-	const char* snapLineModes[4]{"Off", "Center", "Bottom", "Top"};
-	Vector4 minMaxValuesToWrite{0.f, 0.f, 0.f, 0.f};
+	const char* snapLineModes[4]{ "Off", "Center", "Bottom", "Top" };
+	Vector4 minMaxValuesToWrite{ 0.f, 0.f, 0.f, 0.f };
 
 	Vector2 GetSnaplineBase() {
 		switch (Global::pSettings->snapLines.activeMode) {
@@ -25,15 +27,15 @@ namespace Hacks {
 			}
 			case 2:
 			{
-				return Vector2{GetRenderWidth() * 0.5f, static_cast<float>(GetRenderHeight())};
+				return Vector2{ GetRenderWidth() * 0.5f, static_cast<float>(GetRenderHeight()) };
 			}
 			case 3:
 			{
-				return Vector2{GetRenderWidth() * 0.5f, 0.f};
+				return Vector2{ GetRenderWidth() * 0.5f, 0.f };
 			}
 		}
 
-		return Vector2{0.f, 0.f};
+		return Vector2{ 0.f, 0.f };
 	}
 
 	void DoNoRecoil() {
@@ -42,19 +44,19 @@ namespace Hacks {
 		}
 
 		if (Global::pSettings->noRecoil.enabled && Global::gameWorld.GetPlayers().size() > 0) {
-			intptr_t pwa = Memory::ReadValue<intptr_t>(Global::pMemoryInterface, Global::gameWorld.GetPlayers()[0].address + 0x1A0);
+			uint64_t pwa = Memory::ReadValue<uint64_t>(Global::pMemoryInterface, Global::gameWorld.GetPlayers()[0].address + 0x1A0);
 			if (pwa) {
-				intptr_t shootingg = Memory::ReadValue<intptr_t>(Global::pMemoryInterface, pwa + 0x48);
+				uint64_t shootingg = Memory::ReadValue<uint64_t>(Global::pMemoryInterface, pwa + 0x48);
 				if (shootingg) {
 					Memory::Write<float>(Global::pMemoryInterface, shootingg + 0x78, Global::pSettings->noRecoil.shootinggIntensity);
 				}
 
-				intptr_t breath = Memory::ReadValue<intptr_t>(Global::pMemoryInterface, pwa + 0x28);
+				uint64_t breath = Memory::ReadValue<uint64_t>(Global::pMemoryInterface, pwa + 0x28);
 				if (breath) {
 					Memory::Write<float>(Global::pMemoryInterface, breath + 0xA4, Global::pSettings->noRecoil.breathIntensity);
 				}
 
-				intptr_t motion = Memory::ReadValue<intptr_t>(Global::pMemoryInterface, pwa + 0x38);
+				uint64_t motion = Memory::ReadValue<uint64_t>(Global::pMemoryInterface, pwa + 0x38);
 				if (motion) {
 					Memory::Write<float>(Global::pMemoryInterface, motion + 0xD0, Global::pSettings->noRecoil.motionIntensity);
 				}
@@ -69,11 +71,11 @@ namespace Hacks {
 			ImGui::DragFloat("Breath Intensity", &Global::pSettings->noRecoil.breathIntensity, 0.01f, 0.f, 1.f);
 			ImGui::DragFloat("Motion Intensity", &Global::pSettings->noRecoil.motionIntensity, 0.01f, 0.f, 1.f);
 			if (ImGui::Button("Swap Nosway Values")) {
-				intptr_t pwa = Memory::ReadValue<intptr_t>(Global::pMemoryInterface, Global::gameWorld.GetPlayers()[0].address + 0x1A0);
+				uint64_t pwa = Memory::ReadValue<uint64_t>(Global::pMemoryInterface, Global::gameWorld.GetPlayers()[0].address + 0x1A0);
 				if (pwa) {
-					intptr_t walk = Memory::ReadValue<intptr_t>(Global::pMemoryInterface, pwa + 0x30);
+					uint64_t walk = Memory::ReadValue<uint64_t>(Global::pMemoryInterface, pwa + 0x30);
 					if (walk) {
-						intptr_t minMax = Memory::ReadValue<intptr_t>(Global::pMemoryInterface, walk + 0x38);
+						uint64_t minMax = Memory::ReadValue<uint64_t>(Global::pMemoryInterface, walk + 0x38);
 						if (minMax) {
 							Vector4 currentValues = Memory::ReadValue<Vector4>(Global::pMemoryInterface, minMax + 0x20);
 							Memory::Write<Vector4>(Global::pMemoryInterface, minMax + 0x20, minMaxValuesToWrite);
@@ -86,16 +88,36 @@ namespace Hacks {
 
 		if (ImGui::CollapsingHeader("ESP")) {
 			if (ImGui::CollapsingHeader("Snaplines")) {
+				ImGui::PushID("ESP_Snaplines");
 				ImGui::Combo("Mode", &Global::pSettings->snapLines.activeMode, snapLineModes, 4);
 				ImGui::Checkbox("Players", &Global::pSettings->snapLines.types[0]);
 				ImGui::Checkbox("Bosses", &Global::pSettings->snapLines.types[1]);
 				ImGui::Checkbox("AI", &Global::pSettings->snapLines.types[2]);
+				ImGui::PopID();
+			}
+
+			if (ImGui::CollapsingHeader("Box")) {
+				ImGui::PushID("ESP_Box");
+				ImGui::Checkbox("Players", &Global::pSettings->boxESP.types[0]);
+				ImGui::Checkbox("Bosses", &Global::pSettings->boxESP.types[1]);
+				ImGui::Checkbox("AI", &Global::pSettings->boxESP.types[2]);
+				ImGui::DragFloat("Factor", &Global::pSettings->boxESP.factor, 1.f, 1.f);
+				ImGui::PopID();
 			}
 
 			if (ImGui::CollapsingHeader("Skeleton")) {
+				ImGui::PushID("ESP_Skeleton");
 				//ImGui::DragFloat("Close FOV", &Global::pSettings->skeletonESP.closeFOV);
-				ImGui::DragInt("Max Entities", &Global::pSettings->skeletonESP.entities, 1.0f, 1, Global::gameWorld.GetPlayers().size());
+				int* skeletonESPEntitiesCount = &Global::pSettings->skeletonESP.entities;
+				if (ImGui::DragInt("Max Entities", skeletonESPEntitiesCount, 1.f)) {
+					// We don't need to check to see if it is > the amount of people in the world
+					if (*skeletonESPEntitiesCount < 0) {
+						*skeletonESPEntitiesCount = 0;
+					}
+				}
+
 				ImGui::DragFloat("Max Distance", &Global::pSettings->skeletonESP.distance);
+				ImGui::PopID();
 			}
 		}
 	}
@@ -119,6 +141,7 @@ namespace Hacks {
 				continue;
 			}
 
+			bool printedName = false;
 			std::wostringstream stream{};
 			bool isInImportantRange = Vector2Distance(Global::centerScreen, *(Vector2*)&screenPosition) < Global::pSettings->importantRadius;
 			unsigned char playerSide = info.GetSide();
@@ -128,7 +151,8 @@ namespace Hacks {
 				case 2:
 				{
 					if (isInImportantRange) {
-						stream << info.GetNickname() << L"\n";
+						stream << info.GetNickname();
+						printedName = true;
 					}
 
 					break;
@@ -136,27 +160,25 @@ namespace Hacks {
 				case 4:
 				{
 					if (info.GetSettings().GetRole() != 1 && !info.IsPlayer()) {
-						stream << Utils::GetRoleName(info.GetSettings().GetRole()) << L"\n";
+						stream << Utils::GetRoleName(info.GetSettings().GetRole());
+						printedName = true;
 					}
 				}
 			}
 
 			if (isInImportantRange) {
-				ItemTemplate itemTemplate = player.GetActiveWeapon().GetTemplate();
-				if (itemTemplate.address) {
-					const std::wstring templateId = itemTemplate.GetId();
-					if (IS_VALID_WSTRING(templateId)) {
-						if (Global::itemTemplates.contains(templateId)) {
-							stream << Global::itemTemplates[templateId].c_str() << L"\n";
-						}
-						else {
-							stream << L"unlocalized_weapon\n";
-						}
+				if (player.GetProfileInfo().IsPlayer()) {
+					std::wstring id = info.GetGroupHash();
+					if (id != NULL_WSTRING) {
+						stream << L" [" << id << L"]";
 					}
 				}
-				else {
-					stream << L"no_template\n";
+
+				if (printedName) {
+					stream << L"\n";
 				}
+
+				stream << player.GetActiveWeapon().GetTemplate().GetLocalizedName().c_str() << L" ";
 			}
 
 			float test = distance / Global::pSettings->skeletonESP.distance;
@@ -165,127 +187,131 @@ namespace Hacks {
 				test = 1.f;
 
 			stream << std::format("[{:0.0f}m]", distance).c_str();
-			// Convert stream from wchar_t* to char* for raylib
-
-			std::wstring str = stream.str();
-			const size_t bufferSize = (str.size() + 1) * sizeof(char);
-			char* buffer = new char[bufferSize];
-			wcstombs_s(nullptr, buffer, bufferSize, str.c_str(), str.size());
-			Vector2 textSize = MeasureTextEx(GetFontDefault(), buffer, 1.f, 4.5f);
-			if (Global::pSettings->skeletonESP.distance > distance)
-				DrawText(buffer,static_cast<int>(screenPosition.x - textSize.x * 0.5f),static_cast<int>(screenPosition.y - textSize.y * 0.5f),1, ColorAlpha(player.GetColor(localPlayerInfo), test));
-			else if(isInImportantRange)
-				DrawText(buffer, static_cast<int>(screenPosition.x - textSize.x * 0.5f), static_cast<int>(screenPosition.y - textSize.y * 0.5f), 1, ColorAlpha(player.GetColor(localPlayerInfo), .375f));
-			else
-				DrawText(buffer, static_cast<int>(screenPosition.x - textSize.x * 0.5f), static_cast<int>(screenPosition.y - textSize.y * 0.5f), 1, ColorAlpha(player.GetColor(localPlayerInfo), 0.15f));
-
-			delete[] buffer;
+			std::wstring wideString = stream.str();
+			std::string str = std::string(wideString.begin(), wideString.end());
+			Vector2 textSize = MeasureTextEx(GetFontDefault(), str.data(), 1.f, 4.5f);
+			float alphaDistanceMultiplier =
+				Global::pSettings->skeletonESP.distance > distance ? test :
+				isInImportantRange ? 0.375f : 0.15f;
+			DrawText(str.data(), static_cast<int>(screenPosition.x - textSize.x * 0.5f), static_cast<int>(screenPosition.y - textSize.y * 0.5f), 1, ColorAlpha(player.GetColor(localPlayerInfo), alphaDistanceMultiplier));
 			if (Global::pSettings->snapLines.activeMode > 0) {
 				if (info.IsPlayer()) {
-					if (Global::pSettings->snapLines.types[0])
-					{
-						if(!(Global::pSettings->skeletonESP.distance < distance) && !isInImportantRange)
-							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo),test));
-						else if (isInImportantRange)
+					if (Global::pSettings->snapLines.types[0]) {
+						if (!(Global::pSettings->skeletonESP.distance < distance) && !isInImportantRange) {
+							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo), test));
+						}
+						else if (isInImportantRange) {
 							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo), 0.1f));
-						else
+						}
+						else {
 							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo), 0.1f));
+						}
+					}
+
+					if (Global::pSettings->boxESP.types[0]) {
+						Vector3 headPosition = player.GetBone(EBone::Head);
+						Vector3 headScreenPositionV3 = Global::activeCamera.WorldToScreen(headPosition);
+						Vector2 headScreenPosition = *(Vector2*)&headScreenPositionV3;
+						float scale = Global::pSettings->boxESP.factor / distance;
+						Vector2 topLeft = Vector2Subtract(headScreenPosition, Vector2{ scale, 0.f });
+						Vector2 bottomRight = Vector2Add(*(Vector2*)&screenPosition, Vector2{ scale, 0.f });
+						DrawRectangleLines(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y, color);
 					}
 				}
 				else if (info.IsBoss()) {
 					if (Global::pSettings->snapLines.types[1]) {
-						DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(ORANGE,.5f));
+						DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(ORANGE, 0.5f));
+					}
+
+					if (Global::pSettings->boxESP.types[1]) {
+						Vector3 headPosition = player.GetBone(EBone::Head);
+						Vector3 headScreenPositionV3 = Global::activeCamera.WorldToScreen(headPosition);
+						Vector2 headScreenPosition = *(Vector2*)&headScreenPositionV3;
+						float scale = Global::pSettings->boxESP.factor / distance;
+						Vector2 topLeft = Vector2Subtract(headScreenPosition, Vector2{ scale, 0.f });
+						Vector2 bottomRight = Vector2Add(*(Vector2*)&screenPosition, Vector2{ scale, 0.f });
+						DrawRectangleLines(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y, color);
 					}
 				}
 				else {
-					if (Global::pSettings->snapLines.types[2])
-					{
-						if (!(Global::pSettings->skeletonESP.distance < distance) && !isInImportantRange)
-							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo), test/2));
-						else if(isInImportantRange)
+					if (Global::pSettings->snapLines.types[2]) {
+						if (!(Global::pSettings->skeletonESP.distance < distance) && !isInImportantRange) {
+							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo), test * 0.5f));
+						}
+						else if (isInImportantRange) {
 							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo), 0.1f));
-						else
+						}
+						else {
 							DrawLineV(GetSnaplineBase(), *(Vector2*)&screenPosition, ColorAlpha(player.GetColor(localPlayerInfo), 0.1f));
+						}
+					}
+
+					if (Global::pSettings->boxESP.types[2]) {
+						Vector3 headPosition = player.GetBone(EBone::Head);
+						Vector3 headScreenPositionV3 = Global::activeCamera.WorldToScreen(headPosition);
+						Vector2 headScreenPosition = *(Vector2*)&headScreenPositionV3;
+						float scale = Global::pSettings->boxESP.factor / distance;
+						Vector2 topLeft = Vector2Subtract(headScreenPosition, Vector2{ scale, 0.f });
+						Vector2 bottomRight = Vector2Add(*(Vector2*)&screenPosition, Vector2{ scale, 0.f });
+						DrawRectangleLines(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y, color);
 					}
 				}
-			}
+			};
 		}
 	}
-	bool IsCloser(const Player& d1, const Player& d2)
-	{
-		if ((d1.distance < d2.distance) && (d1.distance2d < d2.distance2d) && d1.isInImportantRange)//dude is basically touching you and infront of your mouse
-			return true;
-		else if (d1.distance2d < d2.distance2d && d1.isInImportantRange)//prio inside of mouse range
-			return true;
-		else if (d1.distance < d2.distance && !d1.isInImportantRange)//closer but not in range this |!d1.isInImportantRange| may be a sanity check tbh
-			return true;
-		return false;
-	}
 
-	void DrawSkeletonESP() 
-	{
-		std::vector<Player>& players = Global::gameWorld.GetPlayers();
-		std::vector<Player> playerssorted {};
-		int playerCount = players.size();
-
-		if (playerCount <= 1) 
-		{
+	void DrawSkeletonESP() {
+		if (Global::pSettings->skeletonESP.entities <= 0) {
 			return;
 		}
 
-		float temp = 1.f;
+		std::vector<Player>& players = Global::gameWorld.GetPlayers();
+		std::vector<Player> sortedPlayers{};
+		int playerCount = players.size();
+		if (playerCount <= 1) {
+			return;
+		}
+
 		Vector3 localPlayerPosition = players[0].GetPosition();
 		ProfileInfo& localPlayerInfo = players[0].GetProfileInfo();
-
-		for (int i = 1; i < playerCount; i++) 
-		{
+		for (int i = 1; i < playerCount; i++) {
 			Vector3 worldPosition = players[i].GetPosition();
 			Vector3 screenPosition = Global::activeCamera.WorldToScreen(worldPosition);
-			if (screenPosition.z < 0.1f)
-			{
+			if (screenPosition.z < 0.1f) {
 				continue;
 			}
 
 			/*
-			//small issue with height if close to you and the center of vector2distance being the base of player
-			i tried to move it up with screenpos.y+=3.f all the way to 30.f nothing changed helpppppp
+				small issue with height if close to you and the center of vector2distance being the base of player
+				i tried to move it up with screenpos.y+=3.f all the way to 30.f nothing changed helpppppp
 			*/
 
-			players[i].isInImportantRange = Vector2Distance(Global::centerScreen, *(Vector2*)&screenPosition) < Global::pSettings->importantRadius;
-
+			float fovDistance = Vector2Distance(Global::centerScreen, *(Vector2*)&screenPosition);
+			players[i].isInImportantRange = fovDistance < Global::pSettings->importantRadius;
 			float distance = Vector3Distance(localPlayerPosition, worldPosition);
 			players[i].distance = distance;
-			players[i].distance2d = Vector2Distance(Global::centerScreen, *(Vector2*)&screenPosition);
-
-			if (distance > Global::pSettings->skeletonESP.distance) //if too far don't add to list
-			{
+			players[i].distance2d = fovDistance;
+			if (distance > Global::pSettings->skeletonESP.distance) {
 				continue;
 			}
 
-			playerssorted.push_back(players[i]);//making list of good entities
-
+			sortedPlayers.push_back(players[i]);
 		}
 
-		std::sort(playerssorted.begin(), playerssorted.end(),IsCloser);//now we have absolute closest players 
+		std::sort(sortedPlayers.begin(), sortedPlayers.end(), [](const Player& player1, const Player& player2) {
+			return player1.distance2d < player2.distance2d;
+			});
 
-		if (players.size() < Global::pSettings->skeletonESP.entities)//size sanity check
-			Global::pSettings->skeletonESP.entities = players.size();
-		int ent = Global::pSettings->skeletonESP.entities;
-		if (Global::pSettings->skeletonESP.entities > playerssorted.size())
-			ent = playerssorted.size();
-
-		for (int i = 0; i < ent; ++i)
-		{
-			if (!playerssorted[i].address)
-				break;
-			if (temp <= 0.f)
-				break;
-			playerssorted[i].DrawBones(temp, localPlayerInfo);
-			
-			temp -= (1.f/ent);
+		int entityCount =
+			Global::pSettings->skeletonESP.entities < sortedPlayers.size() ?
+			Global::pSettings->skeletonESP.entities : sortedPlayers.size();
+		float alpha = 1.f;
+		for (int i = 0; i < entityCount; i++) {
+			sortedPlayers[i].DrawBones(alpha, localPlayerInfo);
+			alpha -= 1.f / static_cast<float>(entityCount);
 		}
 	}
-	
+
 	void DrawLootESP() {
 		std::vector<WorldLootItem> loot = Global::gameWorld.GetLoot();
 		for (int i = 0; i < loot.size(); i++) {
@@ -299,16 +325,13 @@ namespace Hacks {
 				continue;
 			}
 
-			std::wstring itemName = Memory::ReadString(Global::pMemoryInterface, Memory::ReadValue<intptr_t>(Global::pMemoryInterface, loot[i].address + 0x68));
+			std::wstring itemName = Memory::ReadString(Global::pMemoryInterface, Memory::ReadValue<uint64_t>(Global::pMemoryInterface, loot[i].address + 0x68));
 			if (Global::itemTemplates.contains(itemName)) {
 				itemName = Global::itemTemplates[itemName];
 			}
 
-			size_t length = itemName.size() + 1;
-			char* buffer = new char[length];
-			wcstombs_s(nullptr, buffer, length, itemName.c_str(), length);
-			DrawText(buffer, screenPosition.x, screenPosition.y, 1, VIOLET);
-			delete[] buffer;
+			std::string drawText(itemName.begin(), itemName.end());
+			DrawText(drawText.data(), screenPosition.x, screenPosition.y, 1, VIOLET);
 		}
 	}
 }
